ec4708d4a8336ae9888ea5de5d48395d13620ab2
LiuHancheng <2463765697@qq.com>
2018-06-26 18:27:55 +0800

    daily maintain

--- api/douban.py
+++ api/douban.py
@@ -1,17 +1,20 @@
-#coding:u8
+# coding:u8
 import requests
-import json 
+import json
 import sys
 import os
 import logging
-Data_logger=logging.getLogger("GetData")
-formatter=logging.Formatter(fmt="%(levelname)s-%(filename)s-%(funcName)s:%(message)s")
-ch=logging.StreamHandler()
+import typing
+Data_logger = logging.getLogger("GetData")
+formatter = logging.Formatter(
+    fmt="%(levelname)s-%(filename)s-%(funcName)s:%(message)s")
+ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 Data_logger.addHandler(ch)
-proxies={ "https":"http://182.92.177.164:2333"}
+proxies = {"https": "http://182.92.177.164:2333"}
 
-def BookInfoByIsbn(ISBN,t_proxies=proxies,apiBaseUrl="https://api.douban.com/v2/book/isbn/"):
+
+def BookInfoByIsbn(ISBN, apiBaseUrl="https://api.douban.com/v2/book/isbn/")->dict:
     '''
     从豆瓣的接口下载图书信息
     Input:
@@ -22,24 +25,16 @@ def BookInfoByIsbn(ISBN,t_proxies=proxies,apiBaseUrl="https://api.douban.com/v2/
         Json file Str
         False
     '''
-    reqobj=requests.get(apiBaseUrl+ISBN,proxies=t_proxies)
+    reqobj = requests.get(apiBaseUrl+ISBN)
+
     if reqobj.status_code != 200:
         Data_logger.warning("远程接口错误")
         return dict({})
-    content=reqobj.text
+    content = reqobj.text
 
     try:
         return json.loads(content)
 
     except json.decoder.JSONDecodeError:
         Data_logger.error("json解析错误")
-        return  {}
-
-#info=get_BookInfoPage(isbn)
-#t_formatjson=json.dumps(json.loads(info),indent=4,sort_keys=False,ensure_ascii=False)
-#print unicode(t_formatjson)
-    
-
-
-
-
+        return {}
